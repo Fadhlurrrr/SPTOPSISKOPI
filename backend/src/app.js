5a44691665ed6dba11343b622ab3af57de10716b
Fadhlur Puasa <fadhlurpuasa@gmail.com>
2026-05-12 09:20:29 +0800

    add railway health check

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,13 +8,20 @@ const scoreRoutes = require('./routes/scoreRoutes')
 
 const app = express()
 
-app.use(cors({
-  origin: "*"
-}))
+app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
 app.get('/', (req, res) => {
-    res.send('Backend SPK TOPSIS is running')
+  res.send('Backend SPK TOPSIS is running')
+})
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' })
 })
 
 app.use('/api/alternatives', alternativeRoutes)
